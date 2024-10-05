f7df48c187115b8fc626c7dce20d4500ee32f622
Shahd Awwad <72280506+shahdaw@users.noreply.github.com>
2024-10-05 22:37:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+
+async function getrecipes(){
+const response =await fetch(`https://forkify-api.herokuapp.com/api/search?q=pizza`);
+const data= await response.json();
+const recipes= data.recipes;
+const result= recipes.map(function(recipe){
+
+return `
+<div class='recipe'>
+<h2>${recipe.title}</h2>
+<img src='${recipe.image_url}'/>
+</div>
+
+`;
+
+}).join('');
+
+document.querySelector(".recipes .row").innerHTML=result;
+
+}
+
+getrecipes();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
